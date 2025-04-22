02b36882185d6d60b5dba7d01da084cff17dc7d0
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-23 00:38:09 +0500

    modifying index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://chat-bot-frontend-l6axfrf0q-mateens-projects-303e07ad.vercel.app",
     credentials: true
 }));
 
