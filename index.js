cd638c22ea19ebb99930b048bf2c94404e5407c4
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-23 14:27:33 +0500

    adding index features

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
-    origin: [ "http://localhost:5173", "https://chat-bot-frontend-zeta.vercel.app" ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    origin: true, // Allow all origins (for testing)
+    credentials: true
+  }));
 
 
 app.use(uploadRoutes);
