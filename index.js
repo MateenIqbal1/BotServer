d61a0dea548fc2372c9c755a10ca2a6956c973da
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-23 13:10:52 +0500

    adding some updates

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "https://chat-bot-frontend-zeta.vercel.app/",
     credentials: true
 }));
 
